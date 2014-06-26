cd49e9fae05d8b76e8a46e77d586fa688319efb7
Lindsey Lee <lb.lee3@yahoo.com>
2014-06-26 16:11:12 -0500

    initial commit

--- spec/entities/game_spec.rb
+++ spec/entities/game_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe 'Game' do
       
   before do
-    @player1 = Player.new()
-    @player2 = Player.new()
+    @player1 = Player.new("Mark")
+    @player2 = Player.new("Lindsey")
     @game = Game.new(@player1, @player2)
   end
 
@@ -12,7 +12,13 @@ describe 'Game' do
     expect(@game).to be_a(Game)
   end
 
+  it "initializes with player1 and player2" do 
+    expect(game.player1).to eq("Mark")
+    expect(game.player2).to eq("Lindsey")
+  end
+
   it "produces a winner" do
+    expect(game.winner).to eq("Mark")
   end
 
 end
